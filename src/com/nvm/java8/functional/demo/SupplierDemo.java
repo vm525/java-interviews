37f9be11b6ec11ad7dfb754a1bebdebd4fc4a971
Vibhor Mittal <vibhor.mittal31@gmail.com>
2021-11-29 01:09:29 +0530

    update SupplierDemo with lambda exp and findAny demo

--- src/com/nvm/java8/functional/demo/SupplierDemo.java
+++ src/com/nvm/java8/functional/demo/SupplierDemo.java
@@ -1,22 +1,20 @@
 package com.nvm.java8.functional.demo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
-public class SupplierDemo implements Supplier<String> {
-    /**
-     * Gets a result.
-     *
-     * @return a result
-     */
-    @Override
-    public String get() {
-        return "Hey, vm525!";
-    }
+public class SupplierDemo {
 
     public static void main(String[] args) {
         // Supplier is used to supply/return a particular value using the `get` abstract method in the FI.
-        // Traditional demo
-        Supplier<String> supplier = new SupplierDemo();
-        System.out.println(supplier.get());
+        // updated with lambda exp.
+        Supplier<String> supplier = () -> "Hey, vm525!";
+
+        // Awesome way we can use this FI, is for a fallback, in case we hit a condition which can't be met in any case.
+        // findAny() method demo with Supplier as parameter.
+        // list is blank => should fallback to the supplier return value from abstract method `get`.
+        List<String> list = Arrays.asList();
+        System.out.println(list.stream().findAny().orElseGet(supplier));
     }
 }
