3f16eac15daf13611b0c4a0868502fc6383835cc
Vibhor Mittal <vibhor.mittal31@gmail.com>
2021-11-29 01:11:34 +0530

    update SupplierDemo with only using lambdas

--- src/com/nvm/java8/functional/demo/SupplierDemo.java
+++ src/com/nvm/java8/functional/demo/SupplierDemo.java
@@ -2,19 +2,15 @@ package com.nvm.java8.functional.demo;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 
 public class SupplierDemo {
 
     public static void main(String[] args) {
         // Supplier is used to supply/return a particular value using the `get` abstract method in the FI.
-        // updated with lambda exp.
-        Supplier<String> supplier = () -> "Hey, vm525!";
-
         // Awesome way we can use this FI, is for a fallback, in case we hit a condition which can't be met in any case.
         // findAny() method demo with Supplier as parameter.
         // list is blank => should fallback to the supplier return value from abstract method `get`.
         List<String> list = Arrays.asList();
-        System.out.println(list.stream().findAny().orElseGet(supplier));
+        System.out.println(list.stream().findAny().orElseGet(() -> "List is empty!"));
     }
 }
