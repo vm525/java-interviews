e64a1859965131510d22b5dc41d316d55b344a39
Vibhor Mittal <vibhor.mittal31@gmail.com>
2021-11-29 00:46:40 +0530

    update predicateDemo with lambda exp and filter demo

--- src/com/nvm/java8/functional/demo/PredicateDemo.java
+++ src/com/nvm/java8/functional/demo/PredicateDemo.java
@@ -1,23 +1,19 @@
 package com.nvm.java8.functional.demo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
-public class PredicateDemo implements Predicate<Integer> {
-
-    /**
-     * Evaluates this predicate on the given argument.
-     *
-     * @param integer the input argument
-     * @return {@code true} if the input argument matches the predicate,
-     * otherwise {@code false}
-     */
-    @Override
-    public boolean test(Integer integer) {
-        return integer % 2 == 0;
-    }
+public class PredicateDemo {
 
     public static void main(String[] args) {
-        PredicateDemo predicate = new PredicateDemo();
-        System.out.println("is num even: " + predicate.test(4));
+        // Predicate FI is used for conditional check.
+        Predicate<Integer> predicate = num -> num % 2 == 0;
+        System.out.println("is num even: " + predicate.test(7));
+
+        // Awesome application is with stream, there is a util method => filter(), which has Predicate FI as method argument
+        // Predicate demo using filter()
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        list.stream().filter(predicate).forEach(even -> System.out.println("printing even num: " + even));
     }
 }
