210012374379fefb8198c2eee4600c32aa454f08
Vibhor Mittal <vibhor.mittal31@gmail.com>
2021-11-28 21:15:29 +0530

    update traditional book example of name based sorting with anonymous method call

--- src/com/nvm/java8/lambda/example/BookService.java
+++ src/com/nvm/java8/lambda/example/BookService.java
@@ -3,26 +3,22 @@ package com.nvm.java8.lambda.example;
 import java.util.Comparator;
 import java.util.List;
 
-//traditional way of using implementing Comparator via a class and using the same in util sort method
+//traditional way of using passing Comparator as anonymous method in util sort method
 
 public class BookService {
 
     public List<Book> getBooksInSortedOrder() {
         List<Book> books = new BookDAO().getBooks();
-        books.sort(new BookNameComparator());
+        books.sort(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o2.getName().compareTo(o1.getName()); //reverse order
+            }
+        });
         return books;
     }
 
     public static void main(String[] args) {
         System.out.println(new BookService().getBooksInSortedOrder());
-    }
-}
-
-
-class BookNameComparator implements Comparator<Book> {
-
-    @Override
-    public int compare(Book o1, Book o2) {
-        return o2.getName().compareTo(o1.getName()); //reverse order
     }
 }
