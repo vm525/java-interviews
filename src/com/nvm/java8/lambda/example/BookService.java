f6094cce45fa6a17bf164f7e8e6bebb6d7f7c71b
Vibhor Mittal <vibhor.mittal31@gmail.com>
2021-11-28 21:17:36 +0530

    update book example of name based sorting with lambda expression awesomeness

--- src/com/nvm/java8/lambda/example/BookService.java
+++ src/com/nvm/java8/lambda/example/BookService.java
@@ -3,18 +3,13 @@ package com.nvm.java8.lambda.example;
 import java.util.Comparator;
 import java.util.List;
 
-//traditional way of using passing Comparator as anonymous method in util sort method
+//single line lambda expr in util sort method
 
 public class BookService {
 
     public List<Book> getBooksInSortedOrder() {
         List<Book> books = new BookDAO().getBooks();
-        books.sort(new Comparator<Book>() {
-            @Override
-            public int compare(Book o1, Book o2) {
-                return o2.getName().compareTo(o1.getName()); //reverse order
-            }
-        });
+        books.sort((o1, o2) -> o2.getName().compareTo(o1.getName())); //reverse order
         return books;
     }
 
