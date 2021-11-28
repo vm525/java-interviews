e26c24b8b8b3d2e9823a817754040a67ba934aad
Vibhor Mittal <vibhor.mittal31@gmail.com>
2021-11-29 00:28:38 +0530

    update with lambda exp and consumer/forEach demo

--- src/com/nvm/java8/functional/demo/ConsumerDemo.java
+++ src/com/nvm/java8/functional/demo/ConsumerDemo.java
@@ -1,20 +1,21 @@
 package com.nvm.java8.functional.demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
-public class ConsumerDemo implements Consumer<Integer> {
-    /**
-     * Performs this operation on the given argument.
-     *
-     * @param integer the input argument
-     */
-    @Override
-    public void accept(Integer integer) {
-        System.out.println("Integer passed: " + integer);
-    }
+public class ConsumerDemo {
 
     public static void main(String[] args) {
-        ConsumerDemo consumer = new ConsumerDemo();
+        // same thing as before, but using lambda exp.
+        Consumer<Integer> consumer = num -> System.out.println("Integer passed: " + num);
         consumer.accept(52);
+
+        //awesome thing is that, in streams, we have forEach method, which accepts Consumer FI as a parameter
+        //forEach Consumer Demo
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        // using the same consumer here from above.
+        list.stream().forEach(consumer); //prints all the numbers present in the list.
     }
 }
