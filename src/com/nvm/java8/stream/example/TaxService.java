d45cfddbd836f4ac928c640e57be1f643a5c116a
Vibhor Mittal <vibhormittal007@gmail.com>
2021-11-30 10:00:54 +0530

    add various approaches of sorting a List via lambdas

--- src/com/nvm/java8/stream/example/TaxService.java
+++ src/com/nvm/java8/stream/example/TaxService.java
@@ -1,5 +1,6 @@
 package com.nvm.java8.stream.example;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +17,13 @@ public class TaxService {
     }
 
     public static void main(String[] args) {
-        System.out.println(evaluateTaxUsers("tax"));
+        List<Employee> taxedEmployees = evaluateTaxUsers("tax");
+        List<Employee> allEmployees = DataBase.getEmployees();
+        //taxedEmployees.stream().sorted(Comparator.comparing(Employee::getId)).forEach(System.out::println);
+        //taxedEmployees.stream().sorted((emp1, emp2) -> emp1.getId() - emp2.getId()).forEach(System.out::println);
+        taxedEmployees.stream().sorted(Comparator.comparing(Employee::getId).thenComparing(Employee::getSalary)).forEach(System.out::println);
+        System.out.println("Salary sorted all Employees list below: ");
+        allEmployees.stream().sorted(Comparator.comparing(Employee::getSalary)).forEach(System.out::println);
+
     }
 }
